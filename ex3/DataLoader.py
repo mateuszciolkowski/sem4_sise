a91a27cf6290dd2b6e2ce3f561efbd8c124d4692
Szymon Kazmierczak <251546@edu.p.lodz.pl>
2025-06-08 17:29:02 +0200

    wczytanie

--- ex3/DataLoader.py
+++ ex3/DataLoader.py
@@ -1,4 +1,4 @@
-import arff
+from scipy.io import arff
 import numpy as np
 from pyexpat import features
 
@@ -7,23 +7,21 @@ from sklearn.model_selection import train_test_split
 
 class RiceDataLoader:
 
+
     @staticmethod
     def load_data(filepath):
-        with open(filepath, 'r') as f:
-            dataset = arff.load(f)
+        data, meta = arff.loadarff(filepath)
 
-        data = list(dataset['data'])
-        attributes = dataset['attributes']
+        data = data.tolist()
+        class_attr_name = meta.names()[-1]
+        class_names = meta[class_attr_name][1]
 
-        class_attr = attributes[-1]
-        class_attr_name = class_attr[0]
-        class_names = class_attr[1]
-
-        class_mapping = {cls_name.decode('utf-8') if isinstance(cls_name, bytes) else cls_name: i
-                         for i, cls_name in enumerate(class_names)}
+        class_mapping = {
+            cls.decode('utf-8') if isinstance(cls, bytes) else cls: i
+            for i, cls in enumerate(class_names)
+        }
 
         X, y = [], []
-
         for row in data:
             features_row = list(row)[:-1]
             label = row[-1]
